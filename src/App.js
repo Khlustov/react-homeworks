5eebdbd59bd13af3d4f71cc3875c16536594863c
Khlustov <khlustov.alex@gmail.com>
2021-03-03 19:39:08 +0300

    feat: add homework #2

--- src/App.js
+++ src/App.js
@@ -3,31 +3,13 @@ import './App.css';
 import {Square} from './components/homework/hw-1'
 import {Text} from './components/homework/hw-1'
 import {First, Huge, Second, Check, Change} from './components/homework/cw-1'
+import {SomeList, SignIn} from './components/homework/hw-2'
 
 function App() {
   return (
     <div>
-      <Square 
-      text = "Big one"
-      cssClass = "big-square"
-      />
-      <Square 
-      text = "Medium"
-      cssClass = "med-square"
-      />
-      <Square 
-      text = "Small one"
-      cssClass = "small-square"
-      />
-      <Text 
-      text = "Some text"
-      />
-      <First text = 'Regular text'/>
-      <Huge />
-      <Second size = '40px' color = 'green' text = 'Green'/>
-      <Second size = '36px' textDecoration = {true} text = 'Underlined'/>
-      <Check />
-      <Change />
+      <SomeList />
+      <SignIn />
     </div>
   );
 }
