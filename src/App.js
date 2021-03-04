957dc5d44f498cc3f2688fe05186c7e94eebe79b
Khlustov <khlustov.alex@gmail.com>
2021-03-04 15:14:05 +0300

    feat: add classwork #2

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import {Square} from './components/homework/hw-1'
 import {Text} from './components/homework/hw-1'
 import {First, Huge, Second, Check, Change} from './components/homework/cw-1'
 import {SomeList, SignIn} from './components/homework/hw-2'
+import {Loader, Element, Dropdown} from './components/homework/cw-2'
 
 function App() {
   return (
     <div>
-      {/* <SomeList /> */}
-      {/* <SignIn /> */}
-      <Change />
+      <Loader />
+      <Element name = {true}/>
+      <Element name = {false}/>
+      <Dropdown />
     </div>
   );
 }
